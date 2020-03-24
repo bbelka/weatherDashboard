357eb2484892b308f229b3a8ee5e4498257403d5
bbtwots <brettbelka@gmail.com>
2020-03-24 14:27:43 -0700

    searching, 5day, current finieshed

---
+++ script.js
@@ -0,0 +1,124 @@
+// var fiveDayQueryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city_name + "&appid=96f595b81fd754dae6e47484245107c2"
+var recentSearchesEl = $("#recentSearches")
+var fiveDayEl = $("#fiveDay");
+var searchFieldEl = $("#searchField");
+var searchBtn = $("#searchBtn");
+var cityEl = $("#city");
+var dateEl = $("#date")
+var currTempEl = $("#currTemp");
+var currHumidEl = $("#currHumid");
+var currWindEl = $("#currWind");
+var currUVIEl = $("#currUVI");
+var lat
+var lon
+var now = moment();
+var nowCheck = now.format('YYYY-MM-DD') + " 15:00:00";
+var x = 0
+
+$(document).ready(function () {
+    console.log("Ready");
+    displayDate();
+})
+
+
+searchBtn.on("click", function () {
+    event.preventDefault();
+    console.log("click");
+    todayForecastSearch();
+
+});
+
+
+// Current weather search
+function todayForecastSearch() {
+    var cityName = searchFieldEl.val().trim();
+    var todayQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=96f595b81fd754dae6e47484245107c2";
+    $.ajax({
+        url: todayQueryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        lat = response.coord.lat;
+        lon = response.coord.lon;
+        var humidity = ("Humidity: " + Math.round(response.main.humidity));
+        var wind = ("Wind speed: " + Math.round(response.wind.speed));
+        var temp = ("Temperature: " + Math.round(response.main.temp));
+        currTempEl.html(temp + '&#8457;');
+        currHumidEl.html(humidity + '%');
+        currWindEl.html(wind + 'mph');
+        cityEl.prepend(response.name + " <br>");
+    }).then(function () {
+        uviSearch();
+        fiveDayForecastSearch()
+    });
+};
+
+function uviSearch() {
+    var uviQueryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=96f595b81fd754dae6e47484245107c2&lat=" + lat + "&lon=" + lon
+    $.ajax({
+        url: uviQueryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var UVI = response.value;
+        currUVIEl.html('UV index: ' + UVI);
+    });
+};
+
+function fiveDayForecastSearch() {
+    // event.preventDefault
+    var cityName = searchFieldEl.val().trim();
+    var fiveDayQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=96f595b81fd754dae6e47484245107c2";
+    $.ajax({
+        url: fiveDayQueryURL,
+        method: "GET"
+    }).then(function (response) {
+        var hour = now.format("H");
+        do {
+            x = x + 1
+            nowCheck = now.add(x, 'd').format('YYYY-MM-DD') + " 15:00:00";
+            for (var i = 0; i < response.list.length; i++) {
+                if (response.list[i].dt_txt === nowCheck) {
+                    var newCol = $("<div class='col-md'>");
+                    var newCard = $('<div class="card text-white bg-info mb-3" style="max-width: 18rem;">');
+                    var newCardHead = $('<div class="card-header">');
+                    var newCardBody = $('<div class="card-body">');
+                    var newCardText = $('<p class="card-text">');
+                    var newTemp = Math.round(response.list[i].main.temp);
+                    var newHumid = response.list[i].main.humidity;
+                    var nowHead = now.format('MMMM D');
+                    newCardHead.append(nowHead);
+                    newCard.append(newCardHead);
+                    newCardText.html('Temp: ' + newTemp + '&#8457;' + '<br>Humidity: ' + newHumid + '%');
+                    newCardBody.append(newCardText);
+                    newCard.append(newCardBody);
+                    newCol.append(newCard);
+                    fiveDayEl.append(newCol);
+                };
+            };
+            nowCheck = now.subtract(x, 'd').format('YYYY-MM-DD') + " 15:00:00";
+        } while (x < 5);
+    });
+};
+
+function displayDate() {
+    dateEl.html(now.format('dddd, MMMM D, YYYY'));
+};
+
+// function currTime() {
+//     $.each($(".description"), function () {
+//         
+//         var timeValue = $(this).attr('value');
+//         if (parseInt(timeValue) < parseInt(hour)) {
+//             $(this).addClass('past');
+//         }
+//         else if (timeValue === hour) {
+//             $(this).addClass('present');
+//         }
+//         else if (parseInt(timeValue) > parseInt(hour)) {
+//             $(this).addClass('future');
+//         };
+//     });
+//     setTimeout(refresh, 60000);
+// };
+// 5-day weather search
